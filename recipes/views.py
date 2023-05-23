eac236d471c3676e033aee6777e66a434e478caf
Isaias Almeida <isaiasalmeida319@gmail.com>
2023-05-23 15:21:55 -0300

    templates

--- recipes/views.py
+++ recipes/views.py
@@ -4,10 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Home')
+    return render(request, 'recipes/home.html')
     
-def sobre(request):
-    return HttpResponse('Sobre')
-
-def contato(request):
-    return HttpResponse('Contato')
