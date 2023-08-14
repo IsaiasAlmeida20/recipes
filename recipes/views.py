d5d47a33add75f167a7b1a1673a74e9cd57ccb1a
Isaias Almeida <isaiasalmeida319@gmail.com>
2023-08-14 09:50:51 -0300

    :recycle: refactor: alter name projeto to core

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
 
 # Create your views here.
 
 def home(request):
+    # recipes = get_list_or_404(
+    #     Recipe.objects.filter(
+    #         is_published=True
+    #     ).order_by('-id')
+    # )
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
@@ -25,7 +30,7 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.get(id=id, is_published=True)
+    recipe = get_object_or_404(Recipe, id=id, is_published=True)
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
