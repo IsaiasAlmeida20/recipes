0685f1b46d7eaa6d515f81e9df6eee19175b4b67
Isaias Almeida <isaiasalmeida319@gmail.com>
2023-08-14 09:25:57 -0300

    :sparkles: feat: render html safe

--- recipes/views.py
+++ recipes/views.py
@@ -25,7 +25,7 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.get(id=id)
+    recipe = Recipe.objects.get(id=id, is_published=True)
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
