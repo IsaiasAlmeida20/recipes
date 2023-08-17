e57c61036f9dbe0a7ba86c3c7977acdbc954c6c0
Isaias Almeida <isaiasalmeida319@gmail.com>
2023-08-17 11:26:06 -0300

    :test_tube: test: test with fixtures

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse, resolve
 from recipes import views
+from recipes.models import Recipe, Category, User
 
 
 class RecipeViewsTest(TestCase):
@@ -21,7 +23,39 @@ class RecipeViewsTest(TestCase):
 
     def test_recipe_home_view_loads_correct_template(self):
         response = self.client.get(reverse('recipes:home'))
-        self.assertTemplateUsed('recipes/pages/home.html', response)
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name='Category')
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='username@email.com',
+        )
+        recipe = Recipe.objects.create(
+            category=category,
+            author=author,
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            cover = SimpleUploadedFile("fake_image.jpg", b"file_content", content_type="image/jpeg"),
+            preparation_steps='Recipe Preparation Steps',
+            preparation_steps_is_html=False,
+            is_published=True,
+        )
+        response = self.client.get(reverse('recipes:home'))
+        response_context = response.context['recipes']
+        response_content = response.content.decode('utf-8')
+        self.assertIsInstance(response_context.first(), Recipe)
+        self.assertEqual(len(response_context), 1)
+        self.assertIn('Recipe Title', response_content)
+
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
