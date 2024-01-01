939f27d37a035d8c108a563b07c3d74e7266782d
Isaias Almeida <isaiasalmeida319@gmail.com>
2024-01-01 20:32:14 -0300

    :test_tube: test: test recipe (one only) not published

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -57,6 +57,11 @@ class RecipeViewsTest(RecipeTestBase):
         response_content = response.content.decode('utf-8')
         self.assertIn(title, response_content)
 
+    def test_recipe_category_template_dont_load_recipe_not_published(self):
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(reverse('recipes:recipe', kwargs={'id': recipe.category.id}))
+        self.assertEqual(404, response.status_code)
+
     def test_recipe_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(views.recipe, view.func)
