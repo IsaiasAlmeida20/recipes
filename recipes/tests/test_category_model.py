2602793779a8799d2fa83ab4d09844deacc91702
Isaias Almeida <isaiasalmeida319@gmail.com>
2024-01-05 21:10:11 -0300

    :test_tube: test: create test to validate max lenght of category name

--- recipes/tests/test_category_model.py
+++ recipes/tests/test_category_model.py
@@ -1,4 +1,4 @@
-from .test_recipe_base import RecipeTestBase, Recipe
+from .test_recipe_base import RecipeTestBase
 from django.core.exceptions import ValidationError
 from parameterized import parameterized
 
@@ -15,3 +15,8 @@ class CategoryModelTest(RecipeTestBase):
             str(self.category), 
             self.category.name
         )
+
+    def test_recipe_category_model_name_max_lenght_is65_chars(self):
+        self.category.name = 'A' * 66
+        with self.assertRaises(ValidationError):
+            self.category.full_clean()
