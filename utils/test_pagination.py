11f7384c43bcd04c37440672378b854a072cafc2
Isaias Almeida <isaiasalmeida319@gmail.com>
2024-01-09 21:58:42 -0300

    test(test_pagination): test to validate if pages show is expected

--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -9,3 +9,29 @@ class PaginationTest(TestCase):
             current_page=1
         )
         self.assertEqual([1,2,3,4], pagination)
+
+    def test_first_range_is_static_if_current_page_is_less_than_middle_page(self):
+        # Current page = 1 - Middle page = 2
+        pagination = make_pagination_range(
+            page_range=list(range(1,21)),
+            qty_pages=4,
+            current_page=1
+        )
+        self.assertEqual([1,2,3,4], pagination)
+
+        # Current page = 2 - Middle page = 2
+        pagination = make_pagination_range(
+            page_range=list(range(1,21)),
+            qty_pages=4,
+            current_page=2
+        )
+        self.assertEqual([1,2,3,4], pagination)
+
+        # Current page = 3 - Middle page = 3
+        # Here change middle page
+        pagination = make_pagination_range(
+            page_range=list(range(1,21)),
+            qty_pages=4,
+            current_page=3
+        )
+        self.assertEqual([2,3,4,5], pagination)
