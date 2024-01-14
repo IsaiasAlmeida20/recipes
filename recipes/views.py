b38eac3a1fcd921db5490fb38cbdcb3b617cfbf7
Isaias Almeida <isaiasalmeida319@gmail.com>
2024-01-13 22:46:18 -0300

    feat(recipes/views.py): appling pagination to home

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404, Http404
 from django.db.models import Q
+from django.core.paginator import Paginator
 from utils.recipes.factory import make_recipe
+
+from utils.pagination import make_pagination_range
 from .models import Recipe
 
 # Create your views here.
@@ -12,8 +15,23 @@ def home(request):
     #     ).order_by('-id')
     # )
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+    paginator = Paginator(recipes, 9)
+    page_obj = paginator.get_page(current_page)
+
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        4,
+        current_page
+    )
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range
     })
 
 
