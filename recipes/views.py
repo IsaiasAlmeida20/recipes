38b11de0436a111e7e8200297b70a736d399f47e
Isaias Almeida <isaiasalmeida319@gmail.com>
2024-01-14 22:00:36 -0300

    refactor(views.py-&-pagination.py): appling pagination to search view

--- recipes/views.py
+++ recipes/views.py
@@ -58,8 +58,13 @@ def search(request):
         ),
         is_published=True
     ).order_by('-id')
+
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
+
     return render(request, 'recipes/pages/search-view.html', {
         'page_title': f'Search for "{search_term}"',
         'search_term': search_term,
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
+        'additional_url_query': f'&q={search_term}'
     })  
